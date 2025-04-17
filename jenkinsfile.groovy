3d79b0843e9068d297fed50993fb80267dcb0af0
Sk93804 <sudheesh.zx@gmail.com>
2025-04-17 21:02:05 +0530

    Update

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,7 +3,9 @@ pipeline {
         branch = '*/main'
         url = 'https://github.com/Sk93804/Maven-tomcat.git'
     }
-
+    options{
+        buildDiscarder(logRotator(numToKeepStr: '2'))
+    }
     stages {
         stage("SCM") {
             agent { label 'slave-01' }  // Agent specific to this stage
