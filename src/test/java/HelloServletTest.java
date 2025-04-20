ddfb7a53ad2b807783a41328a76222fd3f476bca
Sk93804 <sudheesh.zx@gmail.com>
2025-04-20 21:31:39 +0530

    Update

---
+++ src/test/java/HelloServletTest.java
@@ -0,0 +1,12 @@
+package com.microsoft.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloServletTest {
+
+    @Test
+    public void testHelloServletLogic() {
+        assertEquals("Hello", "Hello"); // dummy assertion
+    }
+}
