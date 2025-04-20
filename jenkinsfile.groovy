25ad08e9dc04bd2957b2a73579b04fa66cf28343
Sk93804 <sudheesh.zx@gmail.com>
2025-04-20 15:15:17 +0530

    Update

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,43 +1,21 @@
-pipeline {
-    agent any
-    environment {
-        url = 'https://github.com/Sk93804/Maven-tomcat.git'
-    }
-    tools{
-        maven 'Maven3.8.6'
-    }
-    parameters {
-        choice(name: 'Branch', choices: ['main', 'feature', 'Dev'], description: 'Select the branch to checkout the code')
-    }
-    options {
-        skipDefaultCheckout()
-        buildDiscarder(logRotator(numToKeepStr: '2'))
-    }
-    stages {
-        stage("Checkout") {
-            agent { label 'slave-01' }
-            steps {
-                script {
-                    if (params.Branch == 'main') {
-                        checkout scmGit(
-                            branches: [[name: "*/${params.Branch}"]],
-                            extensions: [],
-                            userRemoteConfigs: [[url: "${env.url}"]]
-                        )
-                        sh 'mvn -v'
-                    } else {
-                        echo "Skipping checkout because Branch = ${params.Branch}"
-                    }
-                }
+pipeline{
+    agent none
+    stages{
+        stage('Checkout'){
+            agent{ label 'slave-01'}
+            steps{
+                checkout scmGit(branches: [[name: '*/main']],
+                 extensions: [], 
+                 userRemoteConfigs: [[url: 'https://github.com/Sk93804/Maven-tomcat.git']])
             }
         }
-
-        stage("Build") {
-            agent { label 'slave-01' }
-            steps {
-                sh 'cd /home/ubuntu/jenkins/workspace/First_job/'
-                sh 'mvn clean package'
+        stage('Build'){
+            agent{ label 'slave-01'}
+            steps{
+               sh 'mvn clean package'
+               sh 'ls -ltr'
+               sh cd './target'
+            }
         }
     }
-}
 }
