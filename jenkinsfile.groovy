654e2a0b7a81b357a1112224600639e0e38ce92c
Sk93804 <sudheesh.zx@gmail.com>
2025-04-21 16:35:58 +0530

    Update

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -46,10 +46,10 @@ pipeline {
             }
         }
         stage('Start SonarQube'){
-            agent { label 'slave-02'}
+            agent { label 'sonar-03'}
             steps{
                 sh ''' echo "Starting sonarQube Container" 
-                docker run -d --name sonarQube  -p 9000:9000 -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true --memory="768m" --memory-swap="1g" -v sonarqube_data:/opt/sonarqube/data  -v sonarqube_logs:/opt/sonarqube/logs sonarqube:latest 
+                docker run -d --name sonarQube  -p 9000:9000 -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true  -v sonarqube_data:/opt/sonarqube/data  -v sonarqube_logs:/opt/sonarqube/logs sonarqube:latest 
                  echo "Waiting for SonarQube to be ready..."
                  sleep 30
 
