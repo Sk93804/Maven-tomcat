c564e71cf2a25d23d3b487b581455b0fa4a4d2cd
Sk93804 <sudheesh.zx@gmail.com>
2025-04-28 16:37:36 +0530

    Update

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -9,8 +9,8 @@ pipeline {
         stage('docker sonar'){
             agent{ label 'sonar-03'}
             steps{
-                // sh 'docker rm  sonarQube'
-                sh 'docker run -d --name sonarQube  -p 9000:9000 -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true --memory="768m" --memory-swap="1g" -v sonarqube_data:/opt/sonarqube/data  -v sonarqube_logs:/opt/sonarqube/logs sonarqube:latest'
+                sh 'docker rm  sonarQube'
+                sh 'docker run -d --name sonarQube  -p 9000:9000 -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -v sonarqube_data:/opt/sonarqube/data  -v sonarqube_logs:/opt/sonarqube/logs sonarqube:latest'
             }
         }
         stage('Checkout') {
