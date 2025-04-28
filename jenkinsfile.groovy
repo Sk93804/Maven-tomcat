cac77791524f56cf507c5ce671a8d023385431df
Sk93804 <sudheesh.zx@gmail.com>
2025-04-28 15:57:23 +0530

    Update

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,10 +1,33 @@
 pipeline{
     agent none
     stages{
-        stage('Cleaning WS'){
+        stage('Code Checkout'){
             agent{ label 'slave-01'}
             steps{
-                cleanWs()
+                 checkout scmGit(branches: [[name: '*/main']],
+                    extensions: [],
+                    userRemoteConfigs: [[url: 'https://github.com/Sk93804/Maven-tomcat.git']])
+            }
+        }
+        stage('sonar analysis'){
+            agent{label 'slave-01'}
+            environment{
+                SONARQUBE = 'MySonar'
+            }
+            withSonarQubeEnv("$SONARQUBE"){
+                sh "mvn sonar:sonar"
+            }
+        }
+        stage('Quality Gate'){
+            steps{
+                timeout(time: 2, unit: 'MINUTES')
+                waitForQualityGate abortPipeline: true
+            }
+            input{
+                message "The Quality Gate as passed pls merge the PR"
+                parameters{
+                    choice(name: 'Merge', choices: ['Yes', 'No'])
+                }
             }
         }
     }
