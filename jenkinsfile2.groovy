aa5924abf938b0b9da7eecae03a58f7603943307
Sk93804 <sudheesh.zx@gmail.com>
2025-06-02 21:09:47 +0530

    Update

--- jenkinsfile2.groovy
+++ jenkinsfile2.groovy
@@ -1,35 +1,40 @@
-pipeline{
-    agent {label 'sonar-03'}
-    options{
+pipeline {
+    agent { label 'sonar-03' }
+
+    options {
         buildDiscarder(logRotator(numToKeepStr: '2'))
     }
-    stages{
-        stage('checkout'){
-             steps{
-                checkout scmGit(branches: [[name: "*/Dev"]], 
-                extensions: [], 
-                userRemoteConfigs: [[url: 'https://github.com/Sk93804/Maven-tomcat.git']])
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scmGit(
+                    branches: [[name: "*/Dev"]],
+                    extensions: [],
+                    userRemoteConfigs: [[url: 'https://github.com/Sk93804/Maven-tomcat.git']]
+                )
 
                 sh 'pwd && ls -ltr'
-             }
-        }
-       stage('sonar analysis'){
-        steps{
-            withSonarQubeEnv('MySonar'){
-                sh 'sonar-scanner'
             }
-        } 
-       }
-       stage('QualityGate'){
-        steps{
-            timeout(time: 2, units:'MINUTES'){
-                def qg = waitForQualityGate()
-                if(qg.status != 'OK'){
-                    error "Pipeline aborted due to quality  gate failure: ${qg.status}"
+        }
+
+        stage('Sonar Analysis') {
+            steps {
+                withSonarQubeEnv('MySonar') {
+                    sh "${tool 'SonarScanner'}/bin/sonar-scanner"
                 }
             }
-        
-       }
-}
-}
+        }
 
+        stage('QualityGate') {
+            steps {
+                timeout(time: 2, unit: 'MINUTES') {
+                    def qg = waitForQualityGate()
+                    if (qg.status != 'OK') {
+                        error "Pipeline aborted due to Quality Gate failure: ${qg.status}"
+                    }
+                }
+            }
+        }
+    }
+}
