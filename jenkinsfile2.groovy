d24006b83669c185712cc18494ed1191507295bb
Sk93804 <sudheesh.zx@gmail.com>
2025-06-02 15:31:36 +0530

    Update

---
+++ jenkinsfile2.groovy
@@ -0,0 +1,36 @@
+pipeline{
+    agent {label 'sonar-03'}
+    stages{
+        stage('checkout'){
+             steps{
+                checkout scmGit(branches: [[name: "*/main"]], 
+                extensions: [], 
+                userRemoteConfigs: [[url: 'https://github.com/Sk93804/Maven-tomcat.git']])
+             }
+        }
+        stage('Sonar Analysis'){
+            steps{
+                withSonarQubeEnv('MySonar') {
+                    sh 'sonar-scanner'
+                }
+            }
+        }
+        stage('QualityGateCheck'){
+            steps{
+                timeout(time:2 unit: 'MINUTES')
+                waitForQualityGate abortPipeline: true
+            }
+            post{
+                success{
+                    gh pr create --title "Automated PR from Jenkins after Quality Gate Passed" \
+                   --body "This PR was raised automatically after SonarQube Quality Gate passed." \
+                   --base main \
+                   --head Dev
+                }
+                failure{
+                    echo "The pipeline has failed to pass the Quality gate"
+                }
+            }
+        }
+    }
+}
